a7a0d3b911e5a8713edfeb8ea4b28225d0dcae78
Mateus Souza <mateusbispo846@gmail.com>
2022-10-25 15:58:31 -0300

    Testes iniciais com movimentos possivei das peças

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace tabuleiro
 {
-    internal class Peca
+    internal abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -30,5 +30,7 @@ namespace tabuleiro
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
